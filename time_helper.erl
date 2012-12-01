0ede35cc61274dad73fa271c126e11f58bcab444
Ales Guzik <ales.guzik@gmail.com>
2012-12-01 08:57:50 +0300

    Added support for hh:mm time format. Fixed single-digit day display.

--- time_helper.erl
+++ time_helper.erl
@@ -7,7 +7,13 @@ now_to_timestamp({Mega, S, _Micro}) ->
 
 date_and_time_to_timestamp(DateStr, TimeStr) ->
     [YYYY, MM, DD] = string:tokens(DateStr, "-"),
-    [Hh,Mm,Ss] = string:tokens(TimeStr, ":"),
+    [Hh,Mm|Tail] = string:tokens(TimeStr, ":"),
+    Ss = case Tail of
+        [] ->
+            "00";
+        [First|_] ->
+            First
+    end,
     [Year,Month,Day,Hour,Minute,Second] = lists:map(fun list_to_integer/1, [YYYY,MM,DD,Hh,Mm,Ss]),
     DateTime = {{Year,Month,Day},{Hour, Minute, Second}},
     calendar:datetime_to_gregorian_seconds(DateTime) - calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time({0,0,0})).
@@ -15,7 +21,9 @@ date_and_time_to_timestamp(DateStr, TimeStr) ->
 timestamp_to_date_and_time(Timestamp) ->
     Now = {Timestamp div 1000000, Timestamp rem 1000000, 0},
     {{Year, Month, Day},{Hour, Minute, Second}} = calendar:now_to_universal_time(Now),
-    {?f("~w-~s~w-~w",[Year, prepend_with_zero(Month), Month, Day]),
+    {?f("~w-~s~w-~s~w",[Year,
+                        prepend_with_zero(Month), Month,
+                        prepend_with_zero(Day), Day]),
      ?f("~s~w:~s~w:~s~w",[prepend_with_zero(Hour), Hour,
                          prepend_with_zero(Minute), Minute,
                          prepend_with_zero(Second), Second ])}.
